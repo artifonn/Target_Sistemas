a48b68f90fce94f7c63d6c52d72ad676e6ba35df
Sérgio Artifon <artifonn@gmail.com>
2024-09-04 09:46:11 -0300

    Finaliza o desafio 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desafio Target Sistemas
+
+Resoluções técnicas do para o processo seletivo de vaga de estágio na Target Sistemas.
---
+++ assets/desafio_1.js
@@ -0,0 +1,11 @@
+// desafio 1
+
+let INDICE = 13;
+let SOMA = 0;
+let K = 0;
+
+while (k < INDICE) {
+  K += 1;
+  SOMA += K;
+  console.log(SOMA); // SOMA = 91
+}
